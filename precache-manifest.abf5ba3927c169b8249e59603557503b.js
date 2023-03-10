04d034ba0aabfcd6ffa4832efe82d609d093970d
Victoria Struk <struk.victoria@gmail.com>
2023-03-10 23:19:11 +0200

    Updates

---
+++ precache-manifest.abf5ba3927c169b8249e59603557503b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c36899812ad365d13da2",
+    "url": "/kmytiv-school/static/css/main.7e20500d.chunk.css"
+  },
+  {
+    "revision": "c36899812ad365d13da2",
+    "url": "/kmytiv-school/static/js/main.c3689981.chunk.js"
+  },
+  {
+    "revision": "9eeee379aeaff73d3fad",
+    "url": "/kmytiv-school/static/css/1.4b37df6d.chunk.css"
+  },
+  {
+    "revision": "9eeee379aeaff73d3fad",
+    "url": "/kmytiv-school/static/js/1.9eeee379.chunk.js"
+  },
+  {
+    "revision": "fa89b2b4461cdd44b840",
+    "url": "/kmytiv-school/static/js/runtime~main.fa89b2b4.js"
+  },
+  {
+    "revision": "5dac84d1eb8e22ef91f5c3398b97090a",
+    "url": "/kmytiv-school/index.html"
+  }
+];
